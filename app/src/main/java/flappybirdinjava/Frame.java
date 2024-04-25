cd884b8ec58e95642d401213eafa5195443da3c1
L_Hits <leechan753@gmail.com>
2024-04-25 16:48:59 +0900

    Added Pipe

--- app/src/main/java/flappybirdinjava/Frame.java
+++ app/src/main/java/flappybirdinjava/Frame.java
@@ -1,84 +1,132 @@
 package flappybirdinjava;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
-public class Frame extends JFrame
-{
-    //동사 + 명사 + 형용사 or 목적어
-    //변수, 함수 통합
-
-    //Variable(변수)
-    private float sizeMultiply = 1.0f; //프레임 크기 배율을 정해주기 위함
-    private final int ORIGIN_SIZE = 512;    //상수값 프레임 사이즈
-    
-    private BackgroundPanel panBackground = new BackgroundPanel();
+public class Frame extends JFrame {
+    private BackgroundPanel pnlGame = new BackgroundPanel();
     private Timer timer = new Timer();
+    private Timer respawn = new Timer();
     private TimerTask timerTask;
+
+    private static Dimension scrnSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private static Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+    private static int taskBarHeight = (int)( scrnSize.getHeight() - winSize.getHeight() );
+
+    //Components
+    Bird bird = new Bird();
    
-    public Frame()
-    {
-        setTitle("Flappy bird in java");
-		setSize(512,512);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
 
-        setLocationRelativeTo(null); //중앙 표시
-        setMinimumSize(new Dimension(256, 256)); //최소화면 설정
+    //Variable
+    private float sizeMultiply = 1.0f;
+    private final int ORIGIN_SIZE = 512;
 
-        
+    public Frame() {
+        //Initialize
+        setTitle("Flappy Bird In Java");
+        setSize(513, 512);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setMinimumSize( new Dimension(256, 256) );
+        setLayout( new CardLayout() );  //유니티의 prefabs 
+
+        //Game Screen
+        pnlGame.setLayout(null);
+        bird.setLocation(100, 100);
+        bird.setSize(100, 100);
+        pnlGame.add(bird);
 
 
-        add(panBackground);
+        // /* 
+        // pipe_up.setLocation(480, 300);
+        // pipe_up.setSize(20,40 );
+        // pnlGame.add(pipe_up);
 
-        timerTask = new TimerTask() 
-        {
-            
+        // pipe_down.setLocation(480, -200);
+        // pipe_down.setSize(20,40 );
+        // pnlGame.add(pipe_down);
+        // */
+
+
+
+        add(pnlGame, "Game");
+        pnlGame.addMouseListener( new MyMouseListener() );
+
+
+        //Timer
+        timerTask = new TimerTask() {
             @Override
-            public void run()
-            {
-                panBackground.update();
-                
+            public void run() {
+                pnlGame.update();
+
             }
         };
-        
-        timer.scheduleAtFixedRate(timerTask, 0, 10);   //ms 단위
+        timer.scheduleAtFixedRate(timerTask, 0, 10);
 
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
 
-    }
+                respawnPipe();
 
-    public float getSizeMultiply() //GameObject에서 Frame의 크기를 받기위한 get함수
-    {
+            }
+        };
+        respawn.scheduleAtFixedRate(timerTask, 0, 2000);
+
+    } //Constructor
+
+    public float getSizeMultiply() {
         return sizeMultiply;
     }
 
-    public BackgroundPanel getBackgroundPanel()
-    {
-        return panBackground;
+    public int getTaskBarHeight() {
+        return taskBarHeight;
     }
 
     @Override
-    public void paint(Graphics g) //프레임의 변경이 일어날 경우
-    {
-        super.paint(g); //paint에 들어가 있는 모든 정보를 긁어옴
-
+    public void paint(Graphics g) {
+        super.paint(g);
         int width = getWidth();
         int height = getHeight();
 
-        if (width > height) //작은 변 기준으로 화면이 바뀜
-        {
+        if (width > height) {
             setSize(height, height);
-        }else 
-        {
+        }
+        else {
             setSize(width, width);
         }
-
-        //변화된 가로변 : 원래 비율 = 변화할 변 : 1
-        
-        sizeMultiply = (float)getWidth() / (float)ORIGIN_SIZE; //화면 비율을 정해줌
+        sizeMultiply = (float)(getHeight() - taskBarHeight) / (float)(ORIGIN_SIZE - taskBarHeight);
     }
 
-}
+    //Listeners
+    private class MyMouseListener extends MouseAdapter {
+        @Override
+        public void mousePressed(MouseEvent e) {
+            bird.jump();
+        }
+    }
+
+    private void respawnPipe()
+    {
+        Pipe_up pipe_up = new Pipe_up();
+        Pipe_down pipe_down = new Pipe_down();
+
+        pipe_up.setLocation(480, 300);
+        pipe_up.setSize(20,40 );
+        pnlGame.add(pipe_up);
+
+        pipe_down.setLocation(480, -200);
+        pipe_down.setSize(20,40 );
+        pnlGame.add(pipe_down);
+    }
+
+
+    
+} //Frame class
+
+
+
+
--- app/src/main/java/flappybirdinjava/GameObject.java
+++ app/src/main/java/flappybirdinjava/GameObject.java
@@ -1,160 +1,141 @@
 package flappybirdinjava;
 
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.*;
 
-//짜기 전에 생각할 것 ->목표, 주어진 입력, 만들어야 할 출력
+public abstract class GameObject extends JLabel {
+    private final Image image;
+    private final int IMAGE_WIDTH, IMAGE_HEIGHT;
+    protected int x;
+    protected int y;
 
-/*
- * -오버라이딩-
- * 함수의 이름이 동일할 것
- * 함수의 인자가 동일할 것
- * 함수의 반환값이 동일할 것
- * 단, 클래스가 하위 클래스 일것
- * 사용할 때 들일 습관 -> 오버라이딩 쓸 때 그 위에 @Override 쓰자.
- * 이건 오버라이딩이 됐는지 확인해줌
- * 
- * -오버로딩-
- * 함수의 이름이 동일할 것
- * 인자 혹은 반환값이 다를 것
- *  반환값이 다를것
- *  인자의 개수가 다를것
- *  인자의 자료형이 다를것
- * 같은 클래스에 존재할 것
- * 
- * 
- * 추상 클래스, 메소드
- * 반드시 써야할 함수나 변수가 존재하다면 
- * 새로운 메소드나 클래스를 만들 때 그 필수 조건이 걸림
- * 까먹어서 안쓰면 경고 뜸
- */
-public abstract class GameObject extends JPanel
-{
-    abstract void update(); //업데이트가 반드시 필요하니 추상 메소드로 설정.
+    public GameObject(Image image) {
+        super();
+        this.image = image;
+        setOpaque(false);
 
-
-    public GameObject()
-    {
-        setBackground(new Color(255,0,0,0));
+        IMAGE_WIDTH = image.getWidth(null);
+        IMAGE_HEIGHT = image.getHeight(null);
     }
 
-}
-
-class BackgroundPanel extends JPanel    //배경 오브젝트
-{
-    private Image imgBackground = new ImageIcon(Main.getPath("/sprites/background.png")).getImage();
-
-
-    private final int WIDTH = imgBackground.getWidth((this));
-    private final int HEIGHT = imgBackground.getHeight((this));
-    private Bird bird = new Bird();
-
-    public BackgroundPanel()    //BackgroundPanel에 새랑 파이드 등을 붙일거니 이걸 null값을 줌
-    {
-        setLayout(null);
-        
-        bird.setLocation(100, 100);
-        bird.setSize( bird.getWidth(), bird.getHeight() );
-        add(bird);
-
-        addMouseListener( new MyMouseListener() );
-
-    }
-
-    public void update()
-    {
-        bird.update();
-
-       
-
-    }
-    
-
-
-    public void paintComponent(Graphics g) 
-    {
-        super.paintComponent(g);
-
-        Frame frame = Main.getFrame();
-        float sizeMultiply = frame.getSizeMultiply();   //Frame.java에서 SizeMultiply를 가져와서 
-                                                        //GameObject.java에서 사용가능하게 넣어줌. 
-        int fixedWidth = (int)(WIDTH * sizeMultiply);
-        int fixedHeight = (int)(HEIGHT * sizeMultiply);
-
-        
-        for(int i = 0; i < frame.getWidth() / fixedWidth + 1; i++)  //몇번 붙이냐? -> 배경이 부족한 만큼 붙여줌.
-        {
-            //drawImage(그릴 객체, x좌표, y좌표, 가로 너비, 높이, 사용하고있는 물건)
-            g.drawImage(imgBackground, i * fixedWidth, 0, fixedWidth, fixedHeight, this);
-        }
-
-
-        
-    }
-    
-    private class MyMouseListener extends MouseAdapter{
-        @Override
-        public void mousePressed(MouseEvent e)
-        {
-            bird.setJumpPower(-10);
-        }
-    }
-}
-
-class Bird extends GameObject   //새 오브젝트
-{
-    private Image imgBird = new ImageIcon(Main.getPath("/sprites/bird_midflap.png")).getImage();
-
-    private final int WIDTH = imgBird.getWidth((this));
-    private final int HEIGHT = imgBird.getHeight((this));
-    private int jumpPower = -1;
-    private final int MAX_JUMP_POWER = 2;
-    private int y = getY();
-
-    public void update()    //아마 사진이랑 위치를 업데이트 해줄 듯?
-    {
-        y = Main.Clamp( y + jumpPower , getHeight(), Main.getFrame().getBackgroundPanel().getHeight() );
-        setLocation(100, y - getHeight() );
-
-
-        if(jumpPower < MAX_JUMP_POWER) //중력 가속도
-        {
-            jumpPower += 1;
-        }
-
-    }
-
-    public int getWidth() //배경 오브젝트에서 새의 크기를 가져오기 위한 함수. 왜냐? private이기 때문
-    {
-        return WIDTH;
-    }
-    public int getHeight()//배경 오브젝트에서 새의 크기를 가져오기 위한 함수. 왜냐? private이기 때문
-    {
-        return HEIGHT;
-    }
-
-    public void setJumpPower(int jumpPower)
-    {
-        this.jumpPower = jumpPower;
+    public void update() {
+        float sizeMultiply = Main.getSizeMultiply();
+        setSize( (int)(IMAGE_WIDTH * sizeMultiply), (int)(IMAGE_HEIGHT * sizeMultiply) );
     }
 
     @Override
-    public void paintComponent(Graphics g)
-    {
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+        int fixedX = (int)( x * Main.getSizeMultiply() );
+        int fixedY = (int)( y * Main.getSizeMultiply() );
+        super.setLocation(fixedX, fixedY);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Frame frame = Main.getFrame();
-        float sizeMultiply = frame.getSizeMultiply();   //Frame.java에서 SizeMultiply를 가져와서 
-                                                        //GameObject.java에서 사용가능하게 넣어줌. 
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
+} //GameObject class
+
+class BackgroundPanel extends JPanel {
+    private Image imgBackground = new ImageIcon( Main.getPath("/sprites/background.png") ).getImage();
+    private final int WIDTH = imgBackground.getWidth(null);
+    private final int HEIGHT = imgBackground.getHeight(null);
+
+    public void update() {
+        for ( Component k : getComponents() ) {
+            GameObject obj = (GameObject)k;
+            obj.update();
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        float sizeMultiply = Main.getFrame().getSizeMultiply();
         int fixedWidth = (int)(WIDTH * sizeMultiply);
         int fixedHeight = (int)(HEIGHT * sizeMultiply);
 
-        g.drawImage(imgBird, 0, 0, fixedWidth, fixedHeight, this);
+        g.drawImage(imgBackground, 0, 0, fixedWidth, fixedHeight, this);
+    }
+} //BackgroundPanel class
 
-        setSize(fixedWidth,fixedHeight);
-        
+
+class Bird extends GameObject {
+    private final static Image image = new ImageIcon( Main.getPath("/sprites/bird_midflap.png") ).getImage();
+
+    private float jump = 0f;
+    private final float GRAVITY = 3f;
+    private final float G_FORCE = 0.5f;
+
+    public Bird() {
+        super(image);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        if ( jump > -GRAVITY) {
+            jump -= G_FORCE;
+        }
+        else{
+            jump = -GRAVITY;
+        }
+
+        y = Main.clamp( (int)(y - jump), 0, 472 - image.getHeight(null) );
+        setLocation(x, y);
+    }
+
+    public void jump() {
+        jump = 10;
+    }
+} //Bird class
+
+class Pipe extends GameObject
+{
+    private final int _SPEED = 1;
+
+    public Pipe(Image image) {
+        super(image);
+    }
+
+    @Override
+    public void update()
+    {
+        super.update();
+        x -= _SPEED;
+        setLocation(x, y);
+
+        if(x <= -50)
+        {
+            getParent().remove(this);   //내가 있는 add()한 위치까지 찾아가서 지운다.
+        }
 
     }
+}
+
+class Pipe_up extends Pipe
+{
+    private static final Image image = new ImageIcon( Main.getPath("/sprites/pipe_up.png") ).getImage();
+
+    public Pipe_up()
+    {
+        super(image);
+    }
+
+}
+
+class Pipe_down extends Pipe
+{
+    private static final Image image = new ImageIcon( Main.getPath("/sprites/pipe_down.png") ).getImage();
+
+    public Pipe_down()
+    {
+        super(image);
+    }
+
 }
